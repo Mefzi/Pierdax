018ad16c96050b3291a98aca3c0ae80ef3208a39
Mefzi <85768000+Mefzi@users.noreply.github.com>
2021-06-30 12:15:11 +0200

    Add files via upload

--- script.js
+++ script.js
@@ -21,39 +21,85 @@ counting = () => {
 
 }
 
-let article = $('.article');
+// CONTENT //
+let starter = $('.article');
 let gallery = $('.gallery');
 let source = $('.source');
 source.css('display', 'none');
-// let starterStatus = "USED";
-// let galleryStatus = "UNUSED";
-let used = true;
+let news = $('.news');
+let gallery1 = $('.gallery1');
+let gallery2 = $('.gallery2');
+
+// BUTTONS //
+let sButtons = $('.starterOption');
+let gButtons = $('.galleryOption');
+
+const changingToStarter = () => {
+
+    gallery.css('display', 'none');
+    starter.css('display', 'block');
+    source.css('display', 'none');
+    sButtons.css('display', 'grid');
+    gButtons.css('display', 'none');
+
+}
 
 const changingToArticle = () => {
 
-    used = true;
-    if(used == true) {
-        gallery.css('display', 'none');
-        article.css('display', 'block');
-        source.css('display', 'none');
-    }
+    news.css('display','none');
+    starter.css('display','block');
 
 }
+
+const changingToNews = () => {
+
+    news.css('display', 'block');
+    starter.css('display','none');
+
+}
+
 
 const changingToGallery = () => {
 
-    used = false;
-    if(used == false) {
-        article.css('display', 'none');
-        gallery.css('display', 'flex');
-        source.css('display', 'block');
-    }
+    gallery.css('display', 'flex');
+    starter.css('display', 'none');
+    news.css('display', 'none');
+    source.css('display', 'block');
+    sButtons.css('display', 'none');
+    gButtons.css('display', 'grid');
+
 }
 
+const changingToGallery1 = () => {
+
+    gallery1.css('display','flex');
+    gallery2.css('display','none');    
+
+}
+
+const changingToGallery2 = () => {
+
+    gallery1.css('display','none');
+    gallery2.css('display','flex');   
+
+}
+
+// STARTER / GALLERY
 let starterButton = document.querySelector('.starterButton');
-starterButton.addEventListener("click",changingToArticle);
+starterButton.addEventListener("click",changingToStarter);
 let galleryButton = document.querySelector('.galleryButton')
 galleryButton.addEventListener("click",changingToGallery);
 
+// INNER STARTER => ARTICLE / NEWS
+let articleButton = document.querySelector('.articleButton');
+articleButton.addEventListener("click", changingToArticle);
+let newsButton = document.querySelector('.newsButton');
+newsButton.addEventListener("click", changingToNews);
+
+// INNER GALLERY => OTHER GALLERIES
+let beerButton = document.querySelector('.beerButton');
+beerButton.addEventListener("click", changingToGallery1);
+let helltakerButton = document.querySelector('.helltakerButton');
+helltakerButton.addEventListener("click", changingToGallery2);
 
 window.addEventListener("load",counting);
